c05cc024345ac7e003b5919a312d7d9a98946ca1
Luis van den Bussche <luisvdbk312@gmail.com>
2022-06-13 13:29:26 +0200

    setup vue and tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+require('./bootstrap');
+
+import { createApp } from 'vue'
+import axios from 'axios';
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = axios;
+ 
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const app = createApp({});
+
+app.component('admin', require('./components/Admin.vue').default);
+
+app.mount('#app');
